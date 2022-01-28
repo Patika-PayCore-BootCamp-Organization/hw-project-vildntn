f0e43dde9a2a56771b3de413b98b7604e2298829
vildntn <vildant35@gmail.com>
2022-01-29 00:58:53 +0300

    customer model added

---
+++ src/main/java/com/example/Project/model/Customer.java
@@ -0,0 +1,70 @@
+package com.example.Project.model;
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String gender;
+    private String email;
+
+    public Customer(int id, String firstName, String lastName, String phoneNumber, String gender, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.email = email;
+    }
+    public Customer(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
