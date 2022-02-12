f7c89801fb3a11a0da0042fbc0f2a7f17e0b2327
vildntn <vildant35@gmail.com>
2022-02-12 22:04:19 +0300

    Customer controller updated

--- src/main/java/com/example/Project/controller/CustomerController.java
+++ src/main/java/com/example/Project/controller/CustomerController.java
@@ -1,26 +1,33 @@
 package com.example.Project.controller;
 
-import com.example.Project.model.Customer;
+
+import com.example.Project.model.entity.Customer;
+import com.example.Project.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping ("/customer/")
+@RequestMapping ("/api/customer")
 public class CustomerController {
-    private List<Customer> customers=new ArrayList<>();
-    {
-
-        customers.add(new Customer(1,"Will","Smith","1254845654","Male","will.smith@gmail.com"));
-    }
+    @Autowired
+    private CustomerService customerService;
 
     @GetMapping("getAll")
     public List<Customer> getAllCustomers() {
-        return customers;
+        return customerService.getAllCustomers();
     }
-    @PostMapping("add")
-    public boolean add(@RequestBody Customer customer) {
-        return customers.add(customer);
+    @PostMapping("/add")
+    public void add(@RequestBody Customer customer) {
+
+        customerService.addCustomer(customer);
     }
-}
+
+        @GetMapping(value="/{id}")
+        public Customer getCustomerById(@PathVariable Integer id){
+            return customerService.getCustomerById(id);
+        }
+
+    }
