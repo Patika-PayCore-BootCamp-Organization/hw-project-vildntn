07a7871e0166011bfe43ade8ef0070b264c130d0
vildntn <vildant35@gmail.com>
2022-02-27 13:25:38 +0300

    hostel updated

--- src/main/java/com/example/Project/model/entity/Hostel.java
+++ src/main/java/com/example/Project/model/entity/Hostel.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "hostel")
+@Table(name = "hostels")
 public class Hostel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
