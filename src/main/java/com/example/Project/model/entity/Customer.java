95a1f50bd44842b7c2fa948c39551c901ffc0210
vildntn <vildant35@gmail.com>
2022-02-27 13:25:29 +0300

    customer updated

--- src/main/java/com/example/Project/model/entity/Customer.java
+++ src/main/java/com/example/Project/model/entity/Customer.java
@@ -3,6 +3,8 @@ package com.example.Project.model.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -14,19 +16,22 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotNull(message = "First name cannot be empty")
     @Column(name = "first_name")
     private String firstName;
 
+    @NotNull(message = "Last name cannot be empty")
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "phone_name")
+    @NotNull(message = "Phone number cannot be empty")
+    @Column(name = "phone_number")
     private String phoneNumber;
 
     @Column(name = "gender")
     private String gender;
 
-
+    @Email
     @Column(name = "email")
     private String email;
 
